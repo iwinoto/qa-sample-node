02461822e22213b390db7cb821e26b8ec8e9bd86
Iwan Winoto <iwinoto@iwinoto-W530>
2014-10-21 15:52:52 +1100

    Collect answer feedback

--- app.js
+++ app.js
@@ -8,6 +8,7 @@
 var express = require('express');
 var https = require('https');
 var url = require('url');
+var util = require('util');
 
 // setup middleware
 var app = express();
@@ -26,8 +27,9 @@ var appInfo = JSON.parse(process.env.VCAP_APPLICATION || "{}");
 
 // defaults for dev outside bluemix
 var service_url = 'https://gateway.watsonplatform.net/qagw/service';
-var service_username = '40d79010-0fd2-4398-abac-eb262e53fe0d';
-var service_password = 'VNP7CEMfOgC3';
+var service_username = '7b0710cb-8c80-433a-b780-d1042ba8abfc';
+var service_password = 'Z7BPYJwDoBdV';
+var answers, feedback;
 
 // VCAP_SERVICES contains all the credentials of services bound to
 // this application. For details of its content, please refer to
@@ -65,9 +67,12 @@ app.get('/', function(req, res){
 // Handle the form POST containing the question to ask Watson and reply with the answer
 app.post('/', function(req, res){
   
-  // Select healthcare as endpoint 
+  // Set the service endpoint (healthcare or travel)
   //var parts = url.parse(service_url +'/v1/question/healthcare');
-  var parts = url.parse(service_url +'/v1/question/travel');
+  //var parts = url.parse(service_url +'/v1/question/travel');
+  console.log("INF", "Requested service = " + req.body.ServiceGroup);
+  var parts = url.parse(service_url +'/v1/question/' + req.body.ServiceGroup);
+  
   // create the request options to POST our question to Watson
   var options = { host: parts.hostname,
     port: parts.port,
@@ -90,9 +95,21 @@ app.post('/', function(req, res){
     });
     
     result.on('end', function() {
-      var answers_pipeline = JSON.parse(response_string),
-          answers = answers_pipeline[0];
-      return res.render('index',{'questionText': req.body.questionText, 'answers': answers})
+      var answers_pipeline = JSON.parse(response_string);
+      answers = answers_pipeline[0];
+      console.log("[INF]", "Watson answers = " + util.inspect(answers_pipeline[0]));
+      console.log("[INF]", "Answers: " + util.inspect(answers.question.answers));
+      console.log("[INF]", "EvidenceList: " + util.inspect(answers.question.evidencelist));
+      feedback = new Array(answers.length);
+      answers.question.answers.forEach(function(answer, index){
+        feedback[index] = {
+            questionId: answers.question.id,
+            answerid : answer.id,
+            feedback : "0",
+            comment : ""
+        };
+      });
+      return res.render('index',{'questionText': req.body.questionText, 'answers': answers, 'feedback': feedback})
     })
 
   });
@@ -116,6 +133,111 @@ app.post('/', function(req, res){
   watson_req.end();
 
 });
+
+app.post('/feedback', function(req, res){
+  // create the request options to POST our question to Watson
+  var parts = url.parse(service_url+'/v1/feedback');
+  var options = {
+    host : parts.hostname,
+    path : parts.pathname,
+    method : 'POST',
+    auth : auth,
+    headers : {
+      'Content-Type' : 'application/json',
+      'Accept' : 'application/json',
+      'X-SyncTimeout' : 30
+     }
+  };
+
+  // create the feedback text to send Watson
+  /*
+  var feedback = {
+    "questionId" : req.body.questionId,
+    "answerid" : req.body.answerId,
+    "feedback" : req.body.feedback
+  };
+  */
+  
+  console.log("[INF]", "Sending feedback index: " + req.body.feedbackIndex +
+      "\n" + util.inspect(feedback));
+  if (feedback[req.body.feedbackIndex].feedback == req.body.feedback){
+    console.log("[WARN]", "Requested feedback is same as current feedback.\n" +
+        "Not sending feedback.");
+    return res.render('index',{'questionText': req.body.questionText, 'answers': answers, 'feedback': feedback});
+  };
+  
+  feedback[req.body.feedbackIndex].feedback = req.body.feedback;
+  console.log("[INF]", "Sending feedback: " + util.inspect(feedback[req.body.feedbackIndex]));
+  
+  var feedback_req = https.request(options, function(result) {
+    result.setEncoding('utf-8');
+    var response_string = '';
+    result.on("data", function(chunk) {
+      response_string += chunk;
+    });
+    
+    result.on('end', function() {
+      console.log("[INF]", "Feedback request end");
+      console.log("[INF]", "Feedback response: " + response_string);
+      console.log("[INF]", "Sending feedback: \n" + util.inspect(feedback));
+      return res.render('index',{'questionText': req.body.questionText, 'answers': answers, 'feedback': feedback});
+    });
+
+    feedback_req.on('error', function(e) {
+      console.log("problem" + e.message);
+    });
+  });
+  
+  feedback_req.write(JSON.stringify(feedback[req.body.feedbackIndex]));
+  feedback_req.end();
+  //return res.render('index',{'questionText': req.body.questionText, 'answers': answers, 'feedback': feedback});
+  //return res.redirect('/', 'index',{'questionText': req.body.questionText, 'answers': answers, 'feedback': feedback});
+});
+
+app.post('/comment', function(req, res){
+  // retrieve the credential information from VCAP_SERVICES for Watson QAAPI
+  var host   = VCAP_SERVICES["QA API Gateway"][0].credentials.uri;
+  var passwd = VCAP_SERVICES["QA API Gateway"][0].credentials.password;
+  var userid = VCAP_SERVICES["QA API Gateway"][0].credentials.username;
+  // create the request options to POST our question to Watson
+  var parts = url.parse(host);
+  var options = {
+    host : parts.hostname,
+    path : parts.pathname+'/v1/feedback',
+    method : 'POST',
+    auth : userid + ":" + passwd,
+    headers : {
+      'Content-Type' : 'application/json',
+      'Accept' : 'application/json',
+      'X-SyncTimeout' : 30
+    }
+  };
+
+  // create the Question text to ask Watson
+  var comment = {
+    "questionId" : req.body.questionId,
+    "answerid" : req.body.answerId,
+    "comment" : req.body.comment
+  };
+
+  var req = https.request(options, function(result) {
+    result.on("data", function(chunk) {
+    res.write(chunk);
+  });
+    result.on('end', function() {
+      res.end();
+    });
+  });
+
+  req.on('error', function(e) {
+  console.log("problem" + e.message);
+  });
+  // Set the POST body and send to Watson
+  req.write(JSON.stringify(comment));
+  req.write("\n\n");
+  req.end();
+});
+
 
 
 // The IP address of the Cloud Foundry DEA (Droplet Execution Agent) that hosts this application:
