c33c3e89cfc992d0e9beef4961402c3db53cf4ab
Iwan Winoto <iwinoto@iwinoto-W530>
2014-11-28 16:50:48 +1100

    Feedback UI in a separate jade file under views/feedback Get example questions for travel and health depending on corpus selection

--- public/js/example_input.js
+++ public/js/example_input.js
@@ -1,5 +1,5 @@
 
-var examples = [
+var health_examples = [
   'What is HIV?',
   'What are the benefits of taking aspirin daily?',
   'What can I do to get more calcium?',
@@ -11,8 +11,26 @@ var examples = [
   'I am at risk for high blood pressure?'
 ];
 
+var travel_examples = [
+                       'Where is the best place to stay in Singapore?',
+                       'What is there to do in The Hague?',
+                       'Where should I eat in KL?',
+                       'Is is safe to travel to Kabul?',
+                       'How do I get downtown from Gare du Nord?',
+                       'What airport should I use to get to London?',
+                       'Where is a ferry from England to Holland?',
+                       'Am I at risk of Hepatitis A in the DRC?',
+                       'Where is the best shopping in Milan?'
+                     ];
+
 function loadExample() {
-  document.getElementById('questionText').value = examples[Math.floor(Math.random()*examples.length)];
+  var service = (document.getElementById('healthService').checked ? 'health' : 'travel');
+  if(service == 'health'){
+    document.getElementById('questionText').value = health_examples[Math.floor(Math.random()*health_examples.length)];
+  }else{
+    document.getElementById('questionText').value = travel_examples[Math.floor(Math.random()*travel_examples.length)];
+  }
+  
 }
 
 //fill and submit the form with a random example
